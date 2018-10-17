26e33c1816e064b540b3925621cb373665679bf3
jfcampos1 <jfcampos1@uc.cl>
2018-10-17 17:54:57 -0300

    db_settings

--- testcoments/testcoments/settings.py
+++ testcoments/testcoments/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'testcoments.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_boss_db',
+        'USER': 'comentariosboss',
+        'PASSWORD': '1234qwer',
+        'HOST': '18.222.175.70',
+        'PORT': '5432',
     }
 }
 
